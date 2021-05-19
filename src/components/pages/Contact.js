bf756a6b496a3103002193c3c040e99b837ae2f3
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-05-19 17:09:45 -0500

    Updated more json scripts as well as finshed app.js router and basic pages

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Contact () {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+
+            <p>
+                Pariatur duis dolore consequat cillum nulla officia anim et mollit mollit. Et ipsum duis id exercitation et duis. Irure culpa velit reprehenderit non consequat Lorem non Lorem Lorem sint. Aute in Lorem ullamco laboris irure officia officia et aliqua adipisicing ullamco eiusmod ut quis.
+            </p>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/components/pages/Resume.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Resume () {
+    return (
+        <div>
+            <h1>Resume Page</h1>
+
+            <p>
+                Pariatur duis dolore consequat cillum nulla officia anim et mollit mollit. Et ipsum duis id exercitation et duis. Irure culpa velit reprehenderit non consequat Lorem non Lorem Lorem sint. Aute in Lorem ullamco laboris irure officia officia et aliqua adipisicing ullamco eiusmod ut quis.
+            </p>
+        </div>
+    )
+}
+
+export default Resume;
