16d0ba318be1a7b246d9e948e3f0e6e29a9c1cdc
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-06-02 00:25:49 -0500

    Fonts still need adjusting, fixed footer for resume page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Typography} from '@material-ui/core';
+// import './style.css';
 
 function Header (props) {
     return(
-        <div>
-            <Typography variant='h2' align='center'>
+        <div >
+            <Typography variant='h2' align='center' id='header'>
                 {props.header}
             </Typography>
         </div>
