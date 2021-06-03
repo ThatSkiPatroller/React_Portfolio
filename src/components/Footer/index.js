40eb53ee2367ed78c86b619bef8e29a66ec6b33a
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-06-03 17:30:17 -0500

    Changed some components to implement more material ui

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,54 @@
 import React from "react";
 import "./style.css"
+import { Container, Grid, Box, Link } from '@material-ui/core';
 
 function Footer () {
         return (
+
+        // <footer>
+        //     <Box
+        //        />
+        //     <Container maxWidth='lg' align='center' >
+        //         <Grid container spacing={5} >
+        //             <Grid item xs={12} sm={12} >
+        //                 <Box borderBottom={1} bgcolor='#9575cd' color='white'>Thanks for viewimg my page!</Box>
+        //                 <Box color='blue' bgcolor='#9575cd'>
+        //                     <Link href='/' >
+        //                         Home
+        //                     </Link>
+        //                 </Box>
+        //                 <Box bgcolor='#9575cd'>
+        //                     <Link href='/Projects' >
+        //                         Projects
+        //                     </Link>
+        //                 </Box>
+        //                 <Box bgcolor='#9575cd'>
+        //                     <Link href='/Contact'  >
+        //                         Contact
+        //                     </Link>
+        //                 </Box>
+        //                 <Box bgcolor='#9575cd'>
+        //                     <Link href='/Resume' >
+        //                         Resume
+        //                     </Link>
+        //                 </Box>
+        //             </Grid>
+        //         </Grid>
+        //         <Box textAlign='center' bgcolor='#9575cd' pt={{xs: 5, sm: 10}} pb={{xs: 5, sm: 0}}>
+        //             Trace Suiter &reg; {new Date().getFullYear()}
+        //         </Box>
+        //     </Container>
+        // </footer>
            
+
+
+
         <footer class="page-footer indigo lighten-1">
         <div class="container">
             <div class="row">
             <div class="col">
                 <h5 class="white-text">Thanks for viewing my page!</h5>
-                <p class="grey-text text-lighten-4">If you liked my page, connect with me through my contact page</p>
+                <p class="grey-text text-lighten-4">If you liked my page, connect with me <br></br>through my contact page</p>
             </div>
             <div class="col">
                 <h5 class="white-text">Links</h5>
@@ -23,7 +62,7 @@ function Footer () {
             </div>
         </div>
         <div class="footer-copyright">
-            <div class="container">
+            <div class="container">  
             © 2014 Trace Suiter Copyright 
             <a class="grey-text text-lighten-4 right" href="#!"></a>
             </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,13 @@
 import React from "react";
 import Header from '../components/Header';
+import Bio from '../components/Bio';
 
 
 function Home () {
     return (
         <div>
             <Header header='Home' />
-
-            <p>     
-            Full stack web developer and graduate of The University of Texas at Austin with a bachelors in Biology, 
-            a minor in French and a Certificate in Full Stack Development. My core skills include JavaScript, CSS, HTML, 
-            Node/React.js as well as an extensive background in teamwork through being part of a patrol unit for the National 
-            Ski Patrol. Decision making is another characteristic I have developed as an Outdoor Emergency Care Technician 
-            where the environment is fast paced and decisions can be life altering. Unique in my healthcare background, I have 
-            come to find a passion in Web Development and Medical Software where I can help people in a different way. 
-             My inclination to always be learning something new has played a big part in my achievements, and I intend to keep 
-             educating myself throughout my future roles.
-            </p>
+            <Bio />
            
         </div>
     )
