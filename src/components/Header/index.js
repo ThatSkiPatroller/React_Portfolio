1edc215c4306a1e4343c4d98d4d332ecf5715c68
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-06-17 11:48:54 -0500

    Fixed header component styling, added padding and font shadow/color change

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Typography} from '@material-ui/core';
-// import './style.css';
+import './style.css';
 
 function Header (props) {
+    const headOf = {
+        color: 'rgb(117, 117, 115)',
+        textShadow: '2px 0 white, 0 2px white, 1px 0 white, 0 1px white'
+    }
+
     return(
-        <div >
-            <Typography variant='h2' align='center' id='header'>
+        <div id='cntnr'>
+            <Typography variant='h2' align='center' id='header' style={headOf}>
                 {props.header}
             </Typography>
         </div>
