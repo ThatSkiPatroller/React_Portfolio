89fb773cd5a1be0e90c29e29274843ec54214df0
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-06-17 12:02:39 -0500

    Went ahead and changed some of the links on contact page to material ui buttons for future development

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Header from '../components/Header';
+import { Button } from '@material-ui/core';
 
 function Contact () {
+  const something = {
+    
+  }
+
     return (
         <div>
         <Header header='Contact Information and Links!' />
-        <div className="collection center">
-          <a href="https://twitter.com/SuiterTrace" className="collection-item blue white-text center">My Twitter Page</a><br />
-          <a href="https://github.com/ThatSkiPatroller" className="collection-item grey black-text center">My Github Page</a><br />
-          <a href="https://linkedin.com/in/trace-suiter" className="collection-item blue darken-4 white-text center">My Linkedin Page</a><br />
-          <a className="collection-item purple white-text">Phone Number: 936-671-3228</a><br />
-          <a className="collection-item teal white-text">E-mail: t.suit@utexas.edu</a>
+        <div className="collection center" style={something}>
+          <Button variant='outlined' href="https://twitter.com/SuiterTrace" className="collection-item blue white-text center">My Twitter Page</Button><br /><br />
+          <Button href="https://github.com/ThatSkiPatroller" className="collection-item grey black-text center">My Github Page</Button><br /><br />
+          <Button href="https://linkedin.com/in/trace-suiter" className="collection-item blue darken-4 white-text center">My Linkedin Page</Button><br /><br />
+          <a className="collection-item purple white-text">Phone Number: 936-671-3228</a><br /><br />
+          <a className="collection-item teal white-text">E-mail: t.suit@utexas.edu</a><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         </div>
         </div>
     )
