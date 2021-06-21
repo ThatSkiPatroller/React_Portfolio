c1a3e25277de9aa5e7cf4ea6547384bc02e6e51b
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-06-21 05:12:52 -0500

    Added react-social-buttons npm package, fixed footer on contact page and made more styling for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,23 +1,65 @@
 import React from "react";
 import Header from '../components/Header';
-import { Button } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
+import { GithubLoginButton, LinkedInLoginButton } from 'react-social-login-buttons';
+import '../pagesStyle/contactStyle.css';
+
 
 function Contact () {
   const something = {
-    
+    backgroundColor: '#9c27b0',
+    color: 'white',
+    height: '50px',
+    marginTop: '10px'
+  }
+
+  const something2 = {
+    backgroundColor: '#ff1744',
+    color: 'white',
+    height: '50px',
+    marginTop: '20px'
+  }
+
+  const something3 = {
+    width: '215px',
+    marginTop: '10px'
+  }
+
+  const something4 = {
+    width: '200px'
+  }
+
+  const margin = {
+    padding: '200px'
+  }
+
+  const margin2 = {
+    padding: '35px'
   }
 
     return (
         <div>
-        <Header header='Contact Information and Links!' />
-        <div className="collection center" style={something}>
-          <Button variant='outlined' href="https://twitter.com/SuiterTrace" className="collection-item blue white-text center">My Twitter Page</Button><br /><br />
-          <Button href="https://github.com/ThatSkiPatroller" className="collection-item grey black-text center">My Github Page</Button><br /><br />
-          <Button href="https://linkedin.com/in/trace-suiter" className="collection-item blue darken-4 white-text center">My Linkedin Page</Button><br /><br />
-          <a className="collection-item purple white-text">Phone Number: 936-671-3228</a><br /><br />
-          <a className="collection-item teal white-text">E-mail: t.suit@utexas.edu</a><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <Header header='Contact Information and Links!'/> 
+          <div style={margin2} />
+          <div class="gradient-border" id="box">
+            <Grid container justify='center' direction='column' align='center' >
+              <Grid item>
+                <GithubLoginButton onClick={() => window.location = 'https://github.com/ThatSkiPatroller'} style={something4}><span>My Github Page</span></GithubLoginButton>
+              </Grid>
+              <Grid item >
+                <LinkedInLoginButton style={something3} onClick={() => window.location = 'https://linkedin.com/in/trace-suiter'}>My Linkedin Page</LinkedInLoginButton>
+              </Grid>
+              <Grid item>
+                <Button variant='contained' size='large' style={something}>Phone Number: 936-671-3228</Button>
+              </Grid>
+              <Grid item>
+                <Button variant='contained' size='large' style={something2}>E-mail: t.suit@utexas.edu</Button>
+              </Grid>
+            </Grid>
+          </div> 
+          <div style={margin} />
         </div>
-        </div>
+       
     )
 }
 
