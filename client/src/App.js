7e3ea329c0594019424ac2ce9e0d76ad42e80f09
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2023-02-06 08:49:30 -0600

    upper/lowercase folder names

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from  "react-router-dom";
-import Home from './Pages/Home.js';
+import Home from './pages/Home.js';
 import './App.css'
 
 function App () {
