86316de515f41749e658c7a142917da6f37e214f
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2023-02-15 00:50:02 -0600

    Made changes to portfolio component destop and mobile with media queries

--- client/src/component/Portfolio/index.js
+++ client/src/component/Portfolio/index.js
@@ -20,7 +20,7 @@ function Portfolio () {
                     
                     <div class="gallery text-center full-width">
                         
-                        <div class="items width3 graphic">
+                        <div class="items width2 graphic">
                             <div class="item-img">
                                 <img src="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio9.PNG" alt="image"/>
                                 <div class="item-img-overlay valign">
@@ -28,14 +28,14 @@ function Portfolio () {
                                         <h6>Bootcamp Project 1</h6>
                                         <p>In this project I collaborated with two other teammates to create an app that gives you a Youtube playlist based on the weather in your current city!</p>
                                     </div>
-                                    <a href="img/portfolio/1.jpg" class="popimg">
+                                    <a href="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio9.PNG" class="popimg">
                                         <i class="icofont icofont-image"></i>
                                     </a>
                                 </div>
                             </div>
                         </div>
                         
-                        <div class="items width3 web">
+                        <div class="items width2 web">
                             <div class="item-img">
                                 <img src="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio3.PNG" alt="image"/>
                                 <div class="item-img-overlay valign">
@@ -43,14 +43,14 @@ function Portfolio () {
                                         <h6>Budget Tracker</h6>
                                         <p>This application allows the user to keep track and manage their budget. It functions as a PWA and works both online and offline with MongoDB</p>
                                     </div>
-                                    <a href="img/portfolio/2.jpg" class="popimg">
+                                    <a href="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio3.PNG" class="popimg">
                                         <i class="icofont icofont-image"></i>
                                     </a>
                                 </div>
                             </div>
                         </div>
                         
-                        <div class="items width3 web">
+                        <div class="items width2 web">
                             <div class="item-img">
                                 <img src="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio7.PNG" alt="image"/>
                                 <div class="item-img-overlay valign">
@@ -58,22 +58,7 @@ function Portfolio () {
                                         <h6>Note Taker</h6>
                                         <p>This application allows the user to write, save, and delete notes through an interactive front-end. Uses express server with GET and POST routes</p>
                                     </div>
-                                    <a href="img/portfolio/3.jpg" class="popimg">
-                                        <i class="icofont icofont-image"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div class="items width4 graphic">
-                            <div class="item-img">
-                                <img src="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio10.PNG" alt="image"/>
-                                <div class="item-img-overlay valign">
-                                    <div class="overlay-info full-width vertical-center">
-                                        <h6>Bootcamp Project 2</h6>
-                                        <p>I collaborated with two other teammates to create Git Jokes which is an app that allows users to post jokes to a twitteresque feed as well as post random jokes to our twitter page from our database.</p>
-                                    </div>
-                                    <a href="img/portfolio/4.jpg" class="popimg">
+                                    <a href="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio7.PNG" class="popimg">
                                         <i class="icofont icofont-image"></i>
                                     </a>
                                 </div>
@@ -88,14 +73,14 @@ function Portfolio () {
                                         <h6>Workout Tracker</h6>
                                         <p>This application allows the user to keep track of their workouts with specific imputs that count time intervals, reps, type of excersize and more</p>
                                     </div>
-                                    <a href="img/portfolio/5.jpg" class="popimg">
+                                    <a href="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio13.PNG" class="popimg">
                                         <i class="icofont icofont-image"></i>
                                     </a>
                                 </div>
                             </div>
                         </div>
                         
-                        <div class="items width3 graphic">
+                        <div class="items width2 graphic">
                             <div class="item-img">
                                 <img src="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio2.PNG" alt="image"/>
                                 <div class="item-img-overlay valign">
@@ -103,7 +88,7 @@ function Portfolio () {
                                         <h6>Project Three</h6>
                                         <p>This final project for the UT Austin Coding Bootcamp works as a car maintenance website where the user can add vehicles, as well as make appointments. This project was a team effort with me and two other developers.</p>
                                     </div>
-                                    <a href="img/portfolio/6.jpg" class="popimg">
+                                    <a href="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio2.PNG" class="popimg">
                                         <i class="icofont icofont-image"></i>
                                     </a>
                                 </div>
@@ -118,14 +103,14 @@ function Portfolio () {
                                         <h6>GraphQL Job Board</h6>
                                         <p>This application is a Job Board site that implements GraphQL as well as Apollo Client with the React.js library. This application is not deployed but can be cloned from my GitHub.</p>
                                     </div>
-                                    <a href="img/portfolio/7.jpg" class="popimg">
+                                    <a href="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio5.PNG" class="popimg">
                                         <i class="icofont icofont-image"></i>
                                     </a>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="items width4 web">
+                        <div class="items width2 web">
                             <div class="item-img">
                                 <img src="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio4.PNG" alt="image"/>
                                 <div class="item-img-overlay valign">
@@ -133,7 +118,22 @@ function Portfolio () {
                                         <h6>GraphQL Chat App</h6>
                                         <p>This is a chat application that uses GraphQL, Apollo, React and WebSocket API to give all the users real-time event-driven responses. This application is not deployed but can be cloned from my GitHub</p>
                                     </div>
-                                    <a href="img/portfolio/7.jpg" class="popimg">
+                                    <a href="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio4.PNG" class="popimg">
+                                        <i class="icofont icofont-image"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="items width4 brand">
+                            <div class="item-img">
+                                <img src="https://d1wedsacc4lfnu.cloudfront.net/PineywoodsSite.png" alt="image"/>
+                                <div class="item-img-overlay valign">
+                                    <div class="overlay-info full-width vertical-center">
+                                        <h6>Pineywoods OBGYN</h6>
+                                        <p>Commercial website I designed and built for a local doctor's office</p>
+                                    </div>
+                                    <a href="https://d1wedsacc4lfnu.cloudfront.net/PineywoodsSite.png" class="popimg">
                                         <i class="icofont icofont-image"></i>
                                     </a>
                                 </div>
@@ -148,7 +148,22 @@ function Portfolio () {
                                         <h6>README.md Generator</h6>
                                         <p>This application runs in the terminal, prompting the user with questions about the project. The answers are used to generate a README.MD file for the user</p>
                                     </div>
-                                    <a href="img/portfolio/7.jpg" class="popimg">
+                                    <a href="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio11.PNG" class="popimg">
+                                        <i class="icofont icofont-image"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="items width3 graphic">
+                            <div class="item-img">
+                                <img src="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio10.PNG" alt="image"/>
+                                <div class="item-img-overlay valign">
+                                    <div class="overlay-info full-width vertical-center">
+                                        <h6>Bootcamp Project 2</h6>
+                                        <p>I collaborated with two other teammates to create Git Jokes which is an app that allows users to post jokes to a twitteresque feed as well as post random jokes to our twitter page from our database.</p>
+                                    </div>
+                                    <a href="https://d1wedsacc4lfnu.cloudfront.net/react_portfolio10.PNG" class="popimg">
                                         <i class="icofont icofont-image"></i>
                                     </a>
                                 </div>
